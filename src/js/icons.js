59dd9e9a70f311af8fdfe40e9e03636d9d76093d
juanruizl <160032371+juanruizl@users.noreply.github.com>
2024-09-27 13:09:12 +0000

    commit_tras_tutorial_con_correcciones_y_perfeccionado

---
+++ src/js/icons.js
@@ -0,0 +1,6 @@
+// icons.js aqui a diferencia del tutorial y el codigo proporcionado actualizo las librerias por las que se usan actualmente para poder importar todo de manera correcta
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,55 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+/**
+ * he esarrollado este proyecto siguiendo las instrucciones proporcionadas, apoyandome en el tutorial/video facilitado por la academia
+ * ademas he adaptado y actualizado algunas partes del codigo para utilizar librerias mas recientes y optimizadas, 
+ * en lugar de las versiones mostradas en el material original, asi aseguraba la compatibilidad y el buen funcionamiento del contador que estaba creando.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
+import 'bootstrap';
+import './icons.js';
+import '../styles/index.css';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+// importo FontAwesome para poder renderizar el icono del reloj
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
-function SimpleCounter() {
-    return (<p>Hello World</p>)
+function SimpleCounter(props) {
+    return (
+        <div className="bigCounter">
+            <div className="calendar">
+                <FontAwesomeIcon icon={faClock} />
+            </div>
+            <div className="four">{props.digitFour % 10}</div>
+            <div className="three">{props.digitThree % 10}</div>
+            <div className="two">{props.digitTwo % 10}</div>
+            <div className="one">{props.digitOne % 10}</div>
+        </div>
+    );
 }
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<SimpleCounter />);
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number.isRequired,
+    digitThree: PropTypes.number.isRequired,
+    digitTwo: PropTypes.number.isRequired,
+    digitOne: PropTypes.number.isRequired
+};
 
+let counter = 0;
+
+const updateCounter = () => {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor((counter / 100) % 10);
+    const two = Math.floor((counter / 10) % 10);
+    const one = Math.floor(counter % 10);
+
+    counter++;
+
+    ReactDOM.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
+        document.querySelector('#app')
+    );
+};
+
+setInterval(updateCounter, 1000);
